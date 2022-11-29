c0d53fa2d3be0be304c6765bcf160d9ec9a31d07
unknown <ganitniduniyanikunj10@gmail.com>
2022-11-29 15:05:50 +0530

    Project Implementation

---
+++ Capstone Project 17-07-2022/CAPSTONE 2022/Frontend App/FrontendApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { CustomersComponent } from './customers/customers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    CustomersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
